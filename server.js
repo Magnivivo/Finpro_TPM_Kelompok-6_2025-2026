52b6a80fbe99f96946cb86d5e3229d95a60eb7f0
Josef <yosefpraditia@gmail.com>
2026-02-07 15:07:56 +0700

    semi niat sore-sore

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 //to test always run [node server.js] in the terminal
 
-const cors = require('cors') // cors untuk akses domain BE-FE
-
 const express = require('express')
 const app = express()
+
+const cors = require('cors') // cors untuk akses domain BE-FE
 
 //list alamat domain yg bisa membuka web
 app.use(cors({
     origin: ['https://localhost:5500', 'https://127.0.0.1:5500']
 }))
 
-//app.get('/about', (req, res) => {}) -> request datanf pada path spesifik bagian function jalankan sebuah respons
-app.get('/', (req, res) => { res.send('Hello from Express') })
+app.use(express.json());
+app.use('/upload', express.static('uploads')) //untuk bagian upload file's
 
-app.get('/about', (req, res) => {res.send('This is the about page')})
-
-//list database
-app.get('/product', (req, res) => {
-    res.json([
-        { id: 1, name: 'ali', division: 'Back End', Team: 'unknown' },
-        { id: 2, name: 'Paijo', division: 'Front-End', Team : 'Redbone'}
-    ])
-} )
+//routes lists
+app.use('/api/auth', authController);
+app.use('/api/admin', adminRoutes);
 
 
-
-app.listen(3000, () => {console.log('Server is running')})
+app.listen(5500, () => {console.log('Server is running')})
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,1 +1,56 @@
 //logika validasi password (huruf besar, kecil, angka, simbol) .
+
+//setup
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const prisma = new PrismaClient();
+const Secret_Key = process.env.JWT_SECRET || "TPM_BNCC_nyawit_Activist"; //secret key -> di .env
+
+// Register Logic
+const register = async (req, res) => {
+    try {
+    // 1. get (Request Body) regist
+    const { 
+      groupName, password, confirmPassword, binusian,
+      fullName, email, whatsapp, lineId, githubId, birthPlace, birthDate 
+        } = req.body;
+        
+    //2.  pasword validation
+    if (password !== savedPassword) {
+        return res.status(400).json({ error: "Password yang dimasukkan tidak sesuai! Masukkan password kembali" });
+        }
+    if (password.length < 8) return res.status(400).json({ error: "Password minimal memiliki 8 karakter" });
+    if (!/[A-Z]/.test(password)) return res.status(400).json({ error: "Password harus memiliki huruf BESAR" });
+    if (!/[a-z]/.test(password)) return res.status(400).json({ error: "Password harus memiliki huruf kecil" });
+    if (!/[0-9]/.test(password)) return res.status(400).json({ error: "Password harus memiliki angka" });
+    if (!/[!@#$%^&*]/.test(password)) return res.status(400).json({ error: "Password harus memiliki simbol" });
+    
+    //3. Cek kesamaan Data -> unique input (Email, WA, LineID)
+    const existingData = await prisma.team.findFirtst({
+        where: {
+        OR: [ { email: email }, { whatsapp: whatsapp }, { lineId: lineId }, { groupName: groupName } ]
+        }    
+    });
+    
+    if (existingUser) {
+        return res.status(400).json({ error: "Nama Tim, Email, WA, atau Line ID sudah terpakai!" });
+    }
+    
+    //4. Password Hashing
+        const hashedPassword = await bcrypt.hash(password, 10);
+    
+    //5. Format in Database
+        const TeamReg = await prisma.team.create({
+            data: {
+                groupName,
+                password: hashedPassword,
+                
+
+            }
+        });
+    
+    }
+
+}
