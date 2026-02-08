6f66085923ab8a489c01911fd96129f68315e2d8
Josef <yosefpraditia@gmail.com>
2026-02-08 07:06:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,26 @@ const cors = require('cors') // cors untuk akses domain BE-FE
 const apiRoutes = require("./src/routes")
 app.use(express.json())
 
+const PORT = process.env.PORT || 5500;
+
 //list alamat domain yg bisa membuka web
 app.use(cors({
-    origin: ['https://localhost:5500', 'https://127.0.0.1:5500']
-}))
+    origin: ['https://localhost:5500', 'https://127.0.0.1:5500'],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use('/upload', express.static('uploads')) //untuk bagian upload file's
 
 //routes lists
+const apiRoutes = require("./src/routes");
 app.use('/api/auth', authController);
 app.use('/api/admin', adminRoutes);
 app.use("/api", apiRoutes)
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
 
 
 app.listen(5500, () => {console.log('Server is running')})
